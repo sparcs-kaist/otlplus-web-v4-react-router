951c1f98ce65a7fcce7ec8d046d082e4ef2213ff
mbp16 <phw1508@naver.com>
2025-08-29 22:40:09 +0900

    fix: update icon color for closed courses

--- app/features/dictionary/components/CourseBlock.tsx
+++ app/features/dictionary/components/CourseBlock.tsx
@@ -1,10 +1,11 @@
-import type { CourseSearchResult } from "@/common/components/interface/CourseSearchResult"
 import styled from "@emotion/styled"
-import Icon from "@/common/components/Icon"
-import themes from "@/styles/themes"
-import FlexWrapper from "@/common/components/FlexWrapper"
-import Typography from "@/common/components/Typography"
 import { useTranslation } from "react-i18next"
+
+import FlexWrapper from "@/common/components/FlexWrapper"
+import Icon from "@/common/components/Icon"
+import Typography from "@/common/components/Typography"
+import type { CourseSearchResult } from "@/common/components/interface/CourseSearchResult"
+import themes from "@/styles/themes"
 
 interface CourseBlockProps {
   course: CourseSearchResult
@@ -16,8 +17,11 @@ const CourseBlockInner = styled.div<{ selected: boolean }>`
   width: 100%;
   border-radius: 7px;
   padding: 8px 10px;
-  border: 1px ${({theme} ) => theme.colors.Background.Block.dark} solid;
-  background-color: ${({ selected, theme }) => selected ? theme.colors.Background.Block.dark : theme.colors.Background.Block.default};
+  border: 1px ${({ theme }) => theme.colors.Background.Block.dark} solid;
+  background-color: ${({ selected, theme }) =>
+    selected
+      ? theme.colors.Background.Block.dark
+      : theme.colors.Background.Block.default};
 `
 
 const Divider = styled.div`
@@ -31,39 +35,70 @@ const DetailHeader = styled(Typography)`
   white-space: nowrap;
 `
 
-const CourseBlock: React.FC<CourseBlockProps> = ({ course, isSelected, selectCourseId }) => {
+const CourseBlock: React.FC<CourseBlockProps> = ({
+  course,
+  isSelected,
+  selectCourseId,
+}) => {
   const { t } = useTranslation()
 
   return (
-    <CourseBlockInner onClick={() => {
-      if (isSelected) {
-        selectCourseId(null);
-      } else {
-        selectCourseId(course.id);
-      }
-    }} selected={isSelected}>
+    <CourseBlockInner
+      onClick={() => {
+        if (isSelected) {
+          selectCourseId(null)
+        } else {
+          selectCourseId(course.id)
+        }
+      }}
+      selected={isSelected}
+    >
       <FlexWrapper direction="row" gap={6} align={"center"}>
-        <Icon type={"Circle"} size={12} color={course.open ? themes.light.colors.Highlight.default : "#CCCCCC"}/>
-        <Typography type={"NormalBold"} color={"Text.default"}>{course.title}</Typography>
-        <Typography type={"Normal"} color={"Text.placeholder"}>{course.code}</Typography>
+        <Icon
+          type={"Circle"}
+          size={12}
+          color={
+            course.open
+              ? themes.light.colors.Highlight.default
+              : themes.light.colors.Text.disable
+          }
+        />
+        <Typography type={"NormalBold"} color={"Text.default"}>
+          {course.title}
+        </Typography>
+        <Typography type={"Normal"} color={"Text.placeholder"}>
+          {course.code}
+        </Typography>
       </FlexWrapper>
-      <Divider/>
+      <Divider />
       <FlexWrapper direction="column" gap={4}>
         <FlexWrapper direction="row" gap={6}>
-          <DetailHeader type={"NormalBold"} color={"Text.default"}>{t('common.class')}</DetailHeader>
-          <Typography type={"Normal"} color={"Text.default"}>{course.department.name}, {course.type}</Typography>
+          <DetailHeader type={"NormalBold"} color={"Text.default"}>
+            {t("common.class")}
+          </DetailHeader>
+          <Typography type={"Normal"} color={"Text.default"}>
+            {course.department.name}, {course.type}
+          </Typography>
         </FlexWrapper>
         <FlexWrapper direction="row" gap={6}>
-          <DetailHeader type={"NormalBold"} color={"Text.default"}>{t('common.professor')}</DetailHeader>
-          <Typography type={"Normal"} color={"Text.default"}>{course.professors.map((professor) => professor.name).join(", ")}</Typography>
+          <DetailHeader type={"NormalBold"} color={"Text.default"}>
+            {t("common.professor")}
+          </DetailHeader>
+          <Typography type={"Normal"} color={"Text.default"}>
+            {course.professors.map((professor) => professor.name).join(", ")}
+          </Typography>
         </FlexWrapper>
         <FlexWrapper direction="row" gap={6}>
-          <DetailHeader type={"NormalBold"} color={"Text.default"}>{t('common.description')}</DetailHeader>
-          <Typography type={"Normal"} color={"Text.default"}>{course.summary}</Typography>
+          <DetailHeader type={"NormalBold"} color={"Text.default"}>
+            {t("common.description")}
+          </DetailHeader>
+          <Typography type={"Normal"} color={"Text.default"}>
+            {course.summary}
+          </Typography>
         </FlexWrapper>
       </FlexWrapper>
     </CourseBlockInner>
-  );
+  )
 }
 
-export default CourseBlock;
+export default CourseBlock
