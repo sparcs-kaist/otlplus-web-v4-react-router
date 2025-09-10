e05dc9ded03da3516ae835eb2774a0538aeea99f
Hyunwoo Park <phw1508@naver.com>
2025-09-10 23:06:10 +0900

    feat: add i18next-browser-languagedetector for language detection (#4)

--- app/libs/i18n/index.ts
+++ app/libs/i18n/index.ts
@@ -1,15 +1,16 @@
 import i18n from "i18next"
+import LanguageDetector from "i18next-browser-languagedetector"
 import { initReactI18next } from "react-i18next"
 
 import { clientEnv } from "@/env"
 import { namespaces, resources } from "@/i18n"
 
 i18n
+  .use(LanguageDetector)
   // pass the i18n instance to react-i18next.
   .use(initReactI18next) // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
-    lng: "ko",
     resources: resources,
     ns: namespaces,
     defaultNS: namespaces[0],
